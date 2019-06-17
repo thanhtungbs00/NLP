df3cbb71a3588dbd93aa6d220157f448f36af482
tung.caothanh <tung.caothanh@dou.com.vn>
2019-06-17 17:12:33 +0700

    update something

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+from __future__ import absolute_import, division, print_function
+
+import argparse
+import collections
+import math
+import os, sys, re
+import random
+from tempfile import gettempdir
+import zipfile
+
 import pandas as pd
 import numpy as np
-import re, os, pprint
+import pprint
+
+from six.moves import urllib
+from six.moves import xrange  # pylint: disable=redefined-builtin
+
+import tensorflow as tf
+from tensorflow.contrib.tensorboard.plugins import projector
+
+data_index = 0
 
 # global variable
 pprint = pprint.pprint
@@ -9,7 +27,7 @@ DATASETS = "datasets/"
 
 def load_Data(path=DATASETS):
     data = []
-    txt_path = os.path.join(path, "Mark.txt")
+    txt_path = os.path.join(path, "Notify.txt")
     file = open(txt_path, "r").readlines()
     for line in file:
         data.append(line)
@@ -41,9 +59,11 @@ def main():
     df = load_Data()
     train_data = df[:10]
     data = pd.DataFrame(train_data, columns = ['text'])
+    print(data)
     data = smooth_Data(data)
     print(data)
     
 
 if __name__ == "__main__":
+    #tf.app.run()
     main()
